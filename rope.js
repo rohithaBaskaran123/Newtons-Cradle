4cc7405a9ae1e634ff92b4cc92dc9ce3d061ef2c
rohithaBaskaran123 <70988626+rohithaBaskaran123@users.noreply.github.com>
2020-11-30 21:54:46 -0800

    Update rope.js

--- rope.js
+++ rope.js
@@ -1,17 +1,19 @@
 class Rope {
-    constructor(bodyA, bodyB){ 
+    constructor(body1, body2,offsetX, offsetY)
+{
+    this.offsetX=offsetX;
+    this.offsetY=offsetY;
    var options = {
-   bodyA: bodyA,
-   bodyB: bodyB,
-   stiffness:0.04,
-   length:10,
+   bodyA: body1,
+   bodyB: body2,
+   pointB : {x:this.offsetX,y:this.offsetY}
    } 
-   this.chain= Constraint.create(options);
-   World.add(world,this.chain);
+   this.rope= Constraint.create(options);
+   World.add(world,this.rope);
    }
    display(){
-       var point1= this.chain.bodyA.position;
-       var point2= this.chain.bodyB.position;
-       line (point1.x, point1.y,point2.x, point2.y);
+       var point1= this.rope.bodyA.position;
+       var point2= this.rope.bodyB.position;
+       line (point1.x, point1.y,point2.x+this.offsetX, point2.y+this.offsetY);
    }
    }
